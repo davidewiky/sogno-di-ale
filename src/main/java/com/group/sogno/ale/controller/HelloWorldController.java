c71ac3e4fcb0d884b128d38bec26e000b091aacc
Consonni Davide <davide.consonni@eoc.ch>
2023-10-22 17:19:40 +0200

    cross origin

--- src/main/java/com/group/sogno/ale/controller/HelloWorldController.java
+++ src/main/java/com/group/sogno/ale/controller/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.group.sogno.ale.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = { "/api" })
+@CrossOrigin(origins = "http://localhost:5173/")
 public class HelloWorldController {
 	
 	@GetMapping("/hello")
